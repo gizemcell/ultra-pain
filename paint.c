e3781a415b80da080e64d41b24d2d63a308fa232
Gizem Celik <gzmck237884@gmail.com>
2026-02-02 00:37:40 +0300

    change brush shape from square to circle

--- paint.c
+++ paint.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <SDL.h>
 #include <stdbool.h>
+#include <math.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 #define TARGET_FPS 60
+#define START_RADIUS 20
+
+void draw_circle(SDL_Surface *surface,int x_center,int y_center,int radius,Uint32 color){
+	SDL_Rect pixel={0,0,1,1};
+	for(int x=x_center-radius;x<x_center+radius;x++){
+		for(int y=y_center-radius;y<y_center+radius;y++){
+			int distance_from_center=sqrt(pow(x-x_center,2)+pow(y-y_center,2));
+			if(distance_from_center<radius){
+				pixel.x=x;
+				pixel.y=y;
+				SDL_FillRect(surface,&pixel,color);
+			}
+
+        }
+	
+	}
+	
+}
 
 int main(){
 	bool done=false;
@@ -26,6 +45,7 @@ int main(){
 	int x;
 	int y;
 	bool draw=false;
+	int radius=START_RADIUS;
 	while(!done){
 		SDL_Event event;
 		while(SDL_PollEvent(&event)){
@@ -39,8 +59,9 @@ int main(){
 			if(draw){
 				x=event.motion.x;
 				y=event.motion.y;
-				SDL_Rect rect={x,y,10,10};
-                		SDL_FillRect(surface,&rect,0x00FFFF00);
+				draw_circle(surface,x,y,radius,0x00FFFF00);
+				//SDL_Rect rect={x,y,10,10};
+                		//SDL_FillRect(surface,&rect,0x00FFFF00);
                 		SDL_UpdateWindowSurface(window);
 			}
 		}
